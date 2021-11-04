c9fb70c3e4b863c2aa25b33821061dedf65c18c0
Oussama Sadik <oussama-sadik@outlook.com>
2021-11-04 16:14:04 +0100

    Update Home page

--- scripts/mainEventHandlers.js
+++ scripts/mainEventHandlers.js
@@ -21,7 +21,38 @@ $(document).ready(function(){
     $('#closeMenu').click(function(){
         closeMenu();
     });
-  
+    
+     //Check for dropdowns
+     let dropdowns = $('.dropdownValued');
+     if(dropdowns.length > 0){
+         initDropdownEventHandlers(dropdowns);
+     }
 
 });
 
+function initDropdownEventHandlers(dropdowns){
+    for(let i =0; i < dropdowns.length; i++){
+        let dropdown = dropdowns[i];
+        setDropdownValue(dropdown);
+        let dropdownElements = dropdown.children[2].children;
+        
+        for (const option of dropdownElements) {
+            option.addEventListener('click', function() {
+                let currentSelected = this.parentNode.parentNode.querySelector('.selected');
+                if(currentSelected != null){
+                    this.parentNode.parentNode.querySelector('.selected').classList.remove('selected');
+                }
+                if (!this.classList.contains('selected')) {
+                    this.classList.add('selected');
+                    setDropdownValue(this.parentNode.parentNode);
+                }
+            })
+        }
+    }
+}
+
+function setDropdownValue(dropdown){
+    console.log(dropdown);
+    let value = dropdown.querySelector('.dropdown-content .selected').innerHTML;
+    dropdown.querySelector('.selectedValue').innerHTML  = value;
+}
